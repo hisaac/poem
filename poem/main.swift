a1bf1e3c5e6e2927f9257c8e2c4a23ea48fe138f
Isaac Halvorson <hello@hisaac.net>
2019-12-24 09:00:33 -0600

    Initial Commit

---
+++ poem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  poem
+//
+//  Created by Isaac Halvorson on 12/24/19.
+//  Copyright © 2019 Levelsoft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
