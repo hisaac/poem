34b00f6c3c01b50bb3d18b9bf97583967dd57d2d
Isaac Halvorson <hello@hisaac.net>
2019-12-24 13:10:36 -0600

    Initial working commit

--- poem/main.swift
+++ poem/main.swift
@@ -2,11 +2,54 @@
 //  main.swift
 //  poem
 //
-//  Created by Isaac Halvorson on 12/24/19.
-//  Copyright © 2019 Levelsoft. All rights reserved.
-//
 
 import Foundation
+import SwiftSoup
 
-print("Hello, World!")
+let requestURL = URL(string: "https://w0.poemhunter.com/members/random-poem/")!
 
+func getRandomPoemHTML() {
+
+	let task = URLSession.shared.dataTask(with: requestURL) { data, response, error in
+		if let error = error {
+			print(error)
+		}
+
+		guard let response = response else { return }
+
+		guard let data = data else { return }
+
+		parseHTML(data: data)
+	}
+
+	task.resume()
+}
+
+func parseHTML(data: Data) {
+	guard let htmlString = String(data: data, encoding: .utf8) else { return }
+	do {
+		let document = try parse(htmlString)
+		let poem = try document.select("div.poem")
+		let title = try poem.select("h2").text()
+		let text = try poem.select("p").html().replacingOccurrences(of: "<br>", with: "\n")
+		let poet = try poem.select("div.poet").text()
+		print("""
+			\(title)
+			by: \(poet)
+			---
+
+			\(text)
+
+			""")
+
+		CFRunLoopStop(CFRunLoopGetCurrent())
+		exit(0)
+	} catch Exception.Error(let type, let message) {
+		print(message)
+	} catch {
+		print("error")
+	}
+}
+
+getRandomPoemHTML()
+CFRunLoopRun()
